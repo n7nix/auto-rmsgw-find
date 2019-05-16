90cc5f40b9a52f61e75614cea91d1539fb9d7aaa
n7nix <basil@pacabunga.com>
2019-05-15 22:22:46 -0700

    Put program path detection at beginning, make check_gateway work.

--- wlgw-check.sh
+++ wlgw-check.sh
@@ -3,6 +3,10 @@
 # Get a list of local RMS Gateways & test a connection
 # This script was developed for a Kenwood TM-V71a
 #
+# Needs these file:
+#  - csv programming file for radio
+#  - generated RMS Gateway list from Winlink Web Services
+#
 # Uncomment this statement for debug echos
 DEBUG=1
 b_dev=true
@@ -58,10 +62,8 @@ function in_path() {
 
     type -P $program_name  &>/dev/null
     if [ $? -ne 0 ] ; then
-        echo "$scriptname: Program: $program_name not found in path ... exiting"
-        exit 1
-#    else
-#       dbgecho "Program: $program_name found"
+        echo "$scriptname: Program: $program_name not found in path ... will exit"
+        retcode=1
     fi
     return $retcode
 }
@@ -147,10 +149,9 @@ function get_chan() {
 # Return memory channel index that radio is using
 function get_mem() {
 
-    dbgecho "get_mem:"
     mem_chan=$(rigctl -r $SERIAL_DEVICE -m $RADIO_MODEL_ID e)
     ret_code=$?
-    echo "Ret code=$ret_code"
+    dbgecho "get_mem: Ret code=$ret_code"
 }
 
 
@@ -159,10 +160,9 @@ function get_mem() {
 # Return frequency that radio is using
 function get_freq() {
 
-    dbgecho "get_freq:"
     freq=$(rigctl -r $SERIAL_DEVICE -m $RADIO_MODEL_ID f)
     ret_code=$?
-    echo "Ret code=$ret_code"
+    dbgecho "get_freq: Ret code=$ret_code"
 }
 
 # ===== function check_radio
@@ -170,7 +170,6 @@ function get_freq() {
 
 function check_radio() {
 
-    in_path rigctl
     mem_chan=$1
 #    get_mem
 
@@ -192,15 +191,49 @@ function check_radio() {
     chan_freq=$(grep -i "Freq:" <<<"$chan_info" | head -n1 | tr -s '[[:space:]] ' | cut -d ' ' -f2)
 }
 
+#===== function check_gateway
+# arg1: readio memory index
+function check_gateway() {
+    mem_chan="$1"
+    gw_call="$2"
+    connect_status=1
+
+    dbgecho "Connect to: $gw_call with radio  memory channel $mem_chan"
+    rigctl -r $SERIAL_DEVICE -m $RADIO_MODEL_ID E $mem_chan
+    if [ "$?" -ne 0 ] ; then
+        echo "Failed to set radio frequency on memory channel $mem_chan for Gateway $gw_call"
+        return $connect_status
+    fi
+    # Connect with paclink-unix
+     wl2kax25 -c "$gw_call"
+     connect_status="$?"
+     return $connect_status
+}
+
 # ==== main
 
 # Temporary to put programs in local bin dir
 dev_setup
 
-# parse command line options
-# if there are no args default to out put a message beacon
-if [[ $# -eq 0 ]] ; then
-   BEACON_TYPE="mesg_beacon"
+# This script uses these programs
+PROGRAM_LIST="rigctl gpsd latlon2grid rmslist.sh"
+b_exitnow=false
+for progname in $PROGRAM_LIST ; do
+    in_path "$progname"
+    retcode=$?
+#    dbgecho "Checking prog: $progname: $retcode"
+    if [ "$retcode" -ne 0 ] ; then
+        b_exitnow=true
+    fi
+done
+
+# Also need the csv programming file, just the NET items.
+if [ ! -e "$DIGI_FREQ_LIST" ] ; then
+    b_exitnow=true
+fi
+
+if $b_exitnow ; then
+    exit 1
 fi
 
 # if there are any args then parse them
@@ -231,14 +264,12 @@ done
 # Was gridsquare set from command line?
 if [ -z "$gridsquare" ] ; then
     # Determine Grid Square location from gps
-    in_path "gpsd"
     get_location
     echo "We are here: lat: $lat$latdir, lon: $lon$londir"
 
     # Assume latlon2grid installed to ~/bin
     # Arguments need to be in decimal degrees ie:
     #  48.48447 -122.901885
-    in_path "latlon2grid"
     gridsquare=$(latlon2grid $lat $lon)
 fi
 dbgecho "Using grid square: $gridsquare"
@@ -250,7 +281,6 @@ if ! $b_dev ; then
     echo
     # Assume rmslist.sh installed to ~/bin
     # rmsglist arg1=distance in miles, arg2=grid square
-    in_path "rmslist.sh"
     rmslist.sh 40 $gridsquare -
 fi
 
@@ -258,28 +288,38 @@ fi
 # Assign some variables
 get_mem
 check_radio $mem_chan
+# Save current memory channel so can restore on exit
 get_freq
-echo "Chan: $mem_chan, name: $chan_name, chan freq: $chan_freq, Frequency: $freq"
+save_mem_chan="$mem_chan"
+echo "Current Chan: $save_mem_chan, name: $chan_name, chan freq: $chan_freq, Frequency: $freq"
 
 # Iterate each line of the RMS Proximity file
 
-printf "\nRMS GW\t    Freq\tDist\tName\tIndex\n"
+gateway_count=0
+connect_count=0
+
+printf "\nRMS GW\t    Freq\tDist\tName\tIndex  ChanStat  ConnStat\n"
 while read fileline ; do
 
     # collapse all spaces
     fileline=$(echo $fileline | tr -s '[[:space:]]')
 
+    # File some variables from Winlink web service call
     distance=$(cut -d' ' -f3 <<< $fileline)
-    freq=$(cut -d' ' -f2 <<< $fileline)
+    wl_freq=$(cut -d' ' -f2 <<< $fileline)
     gw_name=$(cut -d' ' -f1 <<< $fileline)
 
     # Using a TM-V71a 2M/440 dual band radio
-    if (( "$freq" >= 420000000 )) && (( "$freq" < 430000000 )) ; then
+    if (( "$wl_freq" >= 420000000 )) && (( "$wl_freq" < 430000000 )) ; then
         echo "Warning: Frequency violates Line A"
     fi
 
-    freq_name="Not Found"
+    freq_name="unknown"
+    connect_status="n/a"
+
     while read freqlist ; do
+        chan_status="Err"
+        # Get frequency  from csv programming file
         listfreq=$(cut -d',' -f3 <<< $freqlist)
 
         # Get rid of decimal
@@ -287,7 +327,7 @@ while read fileline ; do
         # Pad with trailing spaces to 9 characters
         lstf1=$(printf "%-0.9s" "${lstf1}000000000")
         # Compare frequency in csv file to frequency from Winlink Proximity file
-        if [ "$freq" == "$lstf1" ] ; then
+        if [ "$wl_freq" == "$lstf1" ] ; then
             # Get Radio index from csv file
             radio_index=$(cut -d',' -f1 <<< $freqlist)
             # Get Alpha-numeric name from csv file
@@ -299,22 +339,48 @@ while read fileline ; do
             radfreq=${chan_freq/./}
             # Pad with trailing spaces to 9 characters
             radfreq=$(printf "%-0.9s" "${radfreq}000000000")
-            if [ "$radfreq" == "$freq" ] ; then
-                status_str="OK"
+            # Compare frequency from radio memory channel to frequency from Winlink Proximity file
+            if [ "$radfreq" == "$wl_freq" ] ; then
+                chan_status="OK"
             else
-                status_str="Err"
+                chan_status="Err"
+                radio_index="   "
             fi
 
-#            echo "$freq_name: $listfreq ($lstf1) ($freq) "
+#            echo "$freq_name: $listfreq ($lstf1) ($wl_freq) "
+            # break on match of csv file & Winlink Proximity file
+            break;
         fi
 
     done < $DIGI_FREQ_LIST
 
     # Qualify stations found
     if [ "$distance" != 0 ] && (
-    ( (( "$freq" >= 144000000 )) && (( "$freq" < 148000000 )) ) ||
-    ( (( "$freq" >= 420000000 )) && (( "$freq" < 450000000 )) ) ); then
-        printf "%-10s  %s\t%2s\t%s\t%3s  %s\n"  "$gw_name" "$freq" "$distance" "$freq_name" "$radio_index" "$status_str"
+    ( (( "$wl_freq" >= 144000000 )) && (( "$wl_freq" < 148000000 )) ) ||
+    ( (( "$wl_freq" >= 420000000 )) && (( "$wl_freq" < 450000000 )) ) ); then
+
+        if [ "$chan_status" == "OK" ] ; then
+            gateway_count=$((gateway_count + 1))
+
+            check_gateway $radio_index "$gw_name"
+            if [ "$?" -eq 0 ] ; then
+                connect_count=$((connect_count + 1))
+            fi
+        fi
+        # Debug only: quit or pause after 4 attempts
+        if (( gateway_count > 4 )) ; then
+            break;
+        fi
+    else
+#        dbgecho "Changing channel status to 'Unqaul' for freq: $wl_freq & gateway $gw_name"
+        chan_status="Unqual"
     fi
 
+    printf "%-10s  %s\t%2s\t%s\t%4s   %6s\t%4s\n"  "$gw_name" "$wl_freq" "$distance" "$freq_name" "$radio_index" "$chan_status" "$connect_status"
+
 done < $RMS_PROXIMITY_FILE_OUT
+echo
+echo "Found $gateway_count RMS Gateways, connected: $connect_count"
+echo
+# Set radio back to APRS10
+rigctl -r $SERIAL_DEVICE -m $RADIO_MODEL_ID E $save_mem_chan
