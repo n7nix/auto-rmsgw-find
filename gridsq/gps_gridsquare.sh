9cdb4eb472a42a3bd4c9ef2204a528e50f4c6ece
n7nix <basil@pacabunga.com>
2019-12-26 15:29:02 -0800

    Make gps interrogation a function.

--- gridsq/gps_gridsquare.sh
+++ gridsq/gps_gridsquare.sh
@@ -2,6 +2,8 @@
 #
 # gps_gridsquare.sh
 #
+# Uncomment this statement for debug echos
+#DEBUG=1
 
 # ===== function dbgecho
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
@@ -28,7 +30,35 @@ function get_lat_lon_gpsdsentence() {
     lat=$(echo $gpsdata | cut -d' ' -f1)
     lon=$(echo $gpsdata | cut -d' ' -f2)
 
-#    dbgecho "lat: $lat$latdir, lon: $lon$londir"
+    dbgecho "lat: $lat$latdir, lon: $lon$londir"
+}
+
+# ===== function get_latlon
+function get_latlon() {
+
+    # Verify gpsd is running
+    journalctl --no-pager -u gpsd | tail -n 1 | grep -i error
+    retcode="$?"
+    if [ "$retcode" -eq 0 ] ; then
+        echo "gpsd daemon is not running without errors."
+        return 1
+    fi
+
+    # Verify gpsd is returning sentences
+    is_gps_sentence
+    result=$?
+    dbgecho "Verify gpsd is returning sentences ret: $result"
+
+    if (( result > 0 )) ; then
+        gps_running=true
+
+        get_lat_lon_gpsdsentence
+        if [ "$?" -ne 0 ] ; then
+            echo "Error getting gpsd sentence"
+            return 1
+        fi
+    fi
+    return 0
 }
 
 # ===== main
@@ -37,35 +67,21 @@ function get_lat_lon_gpsdsentence() {
 # Will be set either from command line or gps
 gridsquare=
 
-# Verify gpsd is running
-journalctl --no-pager -u gpsd | tail -n 1 | grep -i error
-retcode="$?"
-if [ "$retcode" -eq 0 ] ; then
-    echo "gpsd daemon is not running without errors."
-    exit 1
+get_latlon
+if [ "$?" -eq 0 ] ; then
+    echo "gps lat/lon is OK!, lat: $lat$latdir, lon: $lon$londir"
+else
+    echo "failed to get lat lon from gps."
+    exit
 fi
-
-# Verify gpsd is returning sentences
-is_gps_sentence
-result=$?
-dbgecho "Verify gpsd is returning sentences ret: $result"
-
-if (( result > 0 )) ; then
-    gps_running=true
-
-    # echo "nmea sentence"
-    get_lat_lon_gpsdsentence
-    if [ "$?" -ne 0 ] ; then
-        echo "Error getting gpsd sentence"
-        exit 1
-    fi
-fi
-
-echo "We are here: lat: $lat$latdir, lon: $lon$londir"
 
 BINDIR="./"
 if [ ! -e "$(pwd)/latlon2grid" ] ; then
     BINDIR="$HOME/bin"
+    if [ ! -e "$BINDIR/latlon2grid" ] ; then
+        echo "Can NOT locate latlon2grid program."
+        exit
+    fi
 fi
 
 # Assume latlon2grid installed to ~/bin
