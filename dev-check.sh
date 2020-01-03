c697c295bcd468128b6fbe16053eb280c5d4e371
n7nix <basil@pacabunga.com>
2020-01-03 09:15:35 -0800

    Check for no ttyUSB devices found.

--- dev-check.sh
+++ dev-check.sh
@@ -4,9 +4,13 @@
 
 SERIAL_DEVICE="/dev/ttyUSB0"
 
+
 # Count number of USB serial devices enumerated
-devcnt=$(ls /dev/ttyUSB* | grep -c "ttyUSB")
+devcnt=$(ls /dev/ttyUSB* 2>/dev/null | grep -c "ttyUSB")
 echo "Found $devcnt USB serial device(s) on this system."
+if [ "$devcnt" = 0 ] ; then
+    exit
+fi
 
 # Get ttyUSB device string
 devstring=$(ls -l /dev/serial/by-id)
